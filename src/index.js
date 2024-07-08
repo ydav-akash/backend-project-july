9b28224ba40a2cce72800b88df003a35a0f85281
ydav-akash <138148832+ydav-akash@users.noreply.github.com>
2024-07-08 18:00:01 +0530

    add

---
+++ src/index.js
@@ -0,0 +1,37 @@
+// require("dotenv").config({ path: "./env" });
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Servr is running at port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO DB connection failed");
+  });
+
+// import express from "express";
+// const app = express();
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.log("ERROR: ", error);
+//       throw error;
+//     });
+
+//     app.listen(process.env.PORT, () => {
+//       console.log(`app is listening on port ${process.env.PORT}`);
+//     });
+//   } catch (error) {
+//     console.error("ERROR: ", error);
+//     throw error;
+//   }
+// })();
