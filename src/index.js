bba0af5140a927d722f309aaa62090208117ad45
ydav-akash <138148832+ydav-akash@users.noreply.github.com>
2024-07-10 18:02:17 +0530

    add register controller

--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@
 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-  path: "./env",
+  path: "./.env",
 });
 
 connectDB()
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-      console.log(`Servr is running at port ${process.env.PORT}`);
+      console.log(`Server is running at port ${process.env.PORT}`);
     });
   })
   .catch((err) => {
